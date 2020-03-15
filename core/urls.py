ecee2f5ddd13bf89833cdecdfbeccfaae57dc9aa
root <root@DESKTOP-6UUT30P.localdomain>
2020-03-15 22:47:18 +0900

    #10 ~ #12 amended  - core/HomeView -> rooms/HomeView

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from rooms import views
 
 app_name = "core"
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.utils import timezone
+from django.views.generic import ListView, DetailView
 from . import models
 
 
 class RoomDetail(DetailView):
     model = models.Room
 
+
+class HomeView(ListView):
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    page_kwargs = "page"
+    ordering = "created_at"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
